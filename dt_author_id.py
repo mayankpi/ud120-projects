e2de6129283c841d4f8639dad490e23bbf4621a6
mayankpi <mayankm522@gmail.com>
2017-04-21 00:42:06 +0530

    changes in files

--- dt_author_id.py
+++ dt_author_id.py
@@ -2,7 +2,6 @@
 
 """ 
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
     Use a Decision Tree to identify emails from the Enron corpus by author:    
     Sara has label 0
     Chris has label 1
@@ -12,8 +11,6 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-from sklearn import tree
-from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,24 +23,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
-
-
-clf = clf.fit(features_train, labels_train)
-
-length = len(features_train[0])
-
-print length
-
-pred = clf.predict(features_test)
-
-print pred
-
-acc = accuracy_score(pred, labels_test)
-
-print acc
 
 #########################################################
-
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,11 +21,49 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
 
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel='rbf', C = 10000)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+count = 0 #count for the number of test in class 1
+
+for a in pred:
+	if a==1:
+		count += 1
+
+print count
+
+# print pred[10]
+# print ""
+# print pred[26]
+# print ""
+# print pred[50]
+
+#####################################################################
+
+#BOTH OF THE ABOVE AND BELOW ARE CORRECT FOR PREDICTING A PARTICULAR VALUE IN TEST SET
+
+#####################################################################
+# pred1 = clf.predict(features_test[10])
+# pred2 = clf.predict(features_test[26])
+# pred3 = clf.predict(features_test[50])
+
+# print pred1
+# print ""
+# print pred2
+# print ""
+# print pred3
+
+
+print accuracy_score(pred, labels_test)
+
 
 #########################################################
 
